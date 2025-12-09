73f274bf35cd0c6943ff52625f8f7a64dc1b3c04
Canvas <peter.martingell@tripledotstudios.com>
2025-12-09 09:46:37 +0000

    Add some Y rotation to randomly shuffle the stack then settle

--- Assets/_Conveyor/Scripts/TAPrototype/TileStackAnimator.cs
+++ Assets/_Conveyor/Scripts/TAPrototype/TileStackAnimator.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using DG.Tweening;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 namespace _2025.ColourBlockArrowProto.Scripts
 {
@@ -27,6 +28,11 @@ namespace _2025.ColourBlockArrowProto.Scripts
         private float[] jumpHeightsPerStackedTile;
         [SerializeField]
         private float[] jumpDurationsPerStackedTile;
+        [Space(10)]
+        [SerializeField]
+        private float randomRotationOffset = 5;
+        [SerializeField]
+        private AnimationCurve rotationCurve = AnimationCurve.Linear(0, 0, 1, 1);
 
         private List<Transform> currentStack = new();
         
@@ -64,6 +70,7 @@ namespace _2025.ColourBlockArrowProto.Scripts
         {
             var durationsCount = jumpDurationsPerStackedTile.Length;
             var distancesCount = jumpHeightsPerStackedTile.Length;
+            var endRotation = stackOrigin.rotation;
             
             for (var i = 0; i < currentStack.Count; i++)
             {
@@ -73,11 +80,17 @@ namespace _2025.ColourBlockArrowProto.Scripts
                 var duration = jumpDurationsPerStackedTile[Mathf.Clamp(i, 0, durationsCount - 1)];
                 var jumpToLocalPoint = rootLocalPoint + jumpLocalDirection * jumpHeightsPerStackedTile[Mathf.Clamp(i, 0, distancesCount - 1)];
 
+                var randomRotation = Quaternion.AngleAxis(Random.Range(-randomRotationOffset, randomRotationOffset), Vector3.up);
+                
                 stackTf.DOKill();
                 
                 stackTf.localPosition = rootLocalPoint;
+                stackTf.localRotation = randomRotation * stackTf.localRotation;
+                
                 stackTf.DOLocalMove(jumpToLocalPoint, duration)
                     .SetEase(jumpCurve);
+                stackTf.DORotateQuaternion(endRotation, duration)
+                    .SetEase(rotationCurve);
             }
         }
     }
